43cad83b62d8de612451d3b6e39bd98eb5ad3160
arendon094 <arendon094@gmail.com>
2017-09-03 18:43:58 -0700

    Implemented use of mysql pool connections. Deleted unecessary code

--- LibraryServer/controllers/fetch.js
+++ LibraryServer/controllers/fetch.js
@@ -1,15 +1,7 @@
 const config = require('../config');
 const mysql = require('mysql');
 
-const connection = mysql.createConnection(config.database);
-
-connection.connect(function(err){
-  if(!err){
-    console.log("Database is connected ... \n\n");
-  } else{
-    console.log("Error connecting to database ... \n\n");
-  }
-});
+const pool = mysql.createPool(config.database);
 
 exports.items = function(req, res, next){
   queryItems(function(err, queryResult){
@@ -28,17 +20,15 @@ exports.itemLogs = function(req, res, next){
 }
 
 function queryItems(callback){
-  connection.query("SELECT * FROM tbl_Item", function(err, rows){
-    callback(err, rows);
-  });
-}
-function queryItem(callback, id){
-  var sql = "SELECT * FROM tbl_Item WHERE ItemID=?";
-  var inserts = [id];
-  sql = mysql.format(sql, inserts);
-
-  connection.query(sql, function(err, rows){
-    callback(err, rows);
+  pool.getConnection(function(err, connection){
+    if(!err){
+      connection.query("SELECT * FROM tbl_Item", function(err, rows){
+        callback(err, rows);
+      });
+      connection.release();
+    } else{
+      console.log("Error connecting to database ... \n\n");
+    }
   });
 }
 
@@ -48,7 +38,14 @@ function queryItemLogs(callback, id){
   sql = mysql.format(sql, inserts);
   var options = { sql: sql, nestTables: true}
 
-  connection.query(options, function(err, rows, fields){
-    callback(err, rows);
+  pool.getConnection(function(err, connection){
+    if(!err){
+      connection.query(options, function(err, rows){
+        callback(err, rows);
+      });
+      connection.release();
+    } else{
+      console.log("Error connecting to database ... \n\n");
+    }
   });
 }
