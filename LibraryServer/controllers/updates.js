1336de44958680818314aa372789aff9dc53c018
arendon094 <arendon094@gmail.com>
2017-09-06 22:40:01 -0700

    Cleaned up updates.js file by reducing the amount of repeated code

--- LibraryServer/controllers/updates.js
+++ LibraryServer/controllers/updates.js
@@ -4,17 +4,20 @@ const dateformat = require('dateformat');
 
 const pool = mysql.createPool(config.database);
 
-// TODO: Clean up file by reducing the amount of repeated code
-
 exports.addItem = function(req, res, next){
   const itemType = req.body.itemType;
   const itemName = req.body.itemName;
+  const additionalInfo = req.body.additionalInfo;
 
   if(!itemType || !itemName){
     return res.status(422).send({ error: 'You must provide an item type and name!' });
   }
 
-  addItemQuery(function(err){
+  var sql = "INSERT INTO tbl_Item(ItemType, ItemName, AdditionalInfo, isAvailable, isOverdue) VALUES (?,?,?,?,?) ";
+  var inserts = [itemType, itemName, additionalInfo, true, false];
+  sql = mysql.format(sql, inserts);
+
+  querySQL(sql, function(err){
     if(err) { return next(err); }
 
     res.end('The item has been added')
@@ -28,30 +31,51 @@ exports.addItemType = function(req, res, next){
     return res.status(422).send({ error: 'You must provide an item type!' });
   }
 
-  addItemTypeQuery(function(err){
+  var sql = "INSERT INTO tbl_ItemType(ItemTypeName) VALUES (?)";
+  var inserts = [itemType];
+  sql = mysql.format(sql, inserts);
+
+  querySQL(sql, function(err){
     if(err) { return next(err); }
 
     res.end('The item type has been added');
-  }, itemType);
+  });
 }
 
 exports.checkOut = function(req, res, next){
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
+  const id = req.params.id;
 
   if(!firstName || !lastName){
     return res.status(422).send({ error: 'You must provide a first and last name!' })
   }
 
-  checkOutQuery(function(err){
+  var curDate = new Date();
+  var datetime = dateformat(curDate, 'yyyy-mm-dd hh:MM:ss');
+  var sql = "INSERT INTO tbl_ItemLog(ItemID, PersonFirstName, PersonLastName, DateBorrowed, DateReturned, hasReturned) VALUES (?,?,?,?,?,?)" +
+  ";UPDATE tbl_Item SET isAvailable=? WHERE ItemID=?";
+  var inserts = [id, firstName, lastName, datetime, '', false, false, id];
+  sql = mysql.format(sql, inserts);
+
+  querySQL(sql, function(err){
     if(err) { return next(err); }
 
     res.end('The item has been checked out');
-  }, req)
+  });
 }
 
 exports.return = function(req, res, next){
-  returnQuery(function(err){
+  const id = req.params.id;
+
+  var curDate = new Date();
+  var datetime = dateformat(curDate, 'yyyy-mm-dd hh:MM:ss');
+  var sql = "UPDATE tbl_ItemLog SET DateReturned=?, hasReturned=? WHERE ItemID=?"
+    + ";UPDATE tbl_Item SET isAvailable=? WHERE ItemID=?";
+  var inserts = [datetime, true, id, true, id];
+  sql = mysql.format(sql, inserts);
+
+  querySQL(sql, function(err){
     if(err) { return next(err); }
 
     res.send('The item has been returned')
@@ -61,23 +85,24 @@ exports.return = function(req, res, next){
 exports.editItem = function(req, res, next){
   const itemType = req.body.itemType;
   const itemName = req.body.itemName;
+  const additionalInfo = req.body.additionalInfo;
 
   if(!itemType || !itemName){
     return res.status(422).send({ error: 'The item name and type cannot be blank!' });
   }
 
-  editItemQuery(function(err){
+  var sql = "UPDATE tbl_Item SET ItemType=?, ItemName=?, AdditionalInfo=? WHERE ItemID=?"
+  var inserts = [itemType, itemName, additionalInfo, req.params.id];
+  sql = mysql.format(sql, inserts);
+
+  querySQL(sql, function(err){
     if(err){ return next(err); }
 
     res.end('The item has been updated');
   }, req);
 }
 
-function addItemQuery(callback, req){
-  var sql = "INSERT INTO tbl_Item(ItemType, ItemName, AdditionalInfo, isAvailable, isOverdue) VALUES (?,?,?,?,?) ";
-  var inserts = [req.body.itemType, req.body.itemName, req.body.additionalInfo, true, false];
-  sql = mysql.format(sql, inserts);
-
+function querySQL(sql, callback){
   pool.getConnection(function(err, connection){
     if(!err){
       connection.query(sql, function(err){
@@ -85,81 +110,7 @@ function addItemQuery(callback, req){
       });
       connection.release();
     } else{
-      console.log('Error connecting to database...\n\n');
-    }
-  });
-}
-
-function addItemTypeQuery(callback, itemType){
-  var sql = "INSERT INTO tbl_ItemType(ItemTypeName) VALUES (?)";
-  var inserts = [itemType];
-  sql = mysql.format(sql, inserts);
-
-  pool.getConnection(function(err, connection){
-    if(!err){
-      connection.query(sql, function(err){
-        callback(err);
-      });
-      connection.release();
-    } else{
-      console.log('Error connecting to database...\n\n')
-    }
-  });
-}
-
-function checkOutQuery(callback, req){
-  var curDate = new Date();
-  var datetime = dateformat(curDate, 'yyyy-mm-dd hh:MM:ss');
-  var sql = "INSERT INTO tbl_ItemLog(ItemID, PersonFirstName, PersonLastName, DateBorrowed, DateReturned, hasReturned) VALUES (?,?,?,?,?,?)" +
-  ";UPDATE tbl_Item SET isAvailable=? WHERE ItemID=?";
-  var inserts = [req.params.id, req.body.firstName, req.body.lastName, datetime, '', false, false, req.params.id];
-  sql = mysql.format(sql, inserts);
-
-  pool.getConnection(function(err, connection){
-    if(!err){
-      connection.query(sql, function(err){
-        callback(err);
-      });
-      connection.release();
-    } else{
-      console.log('Error connecting to database...\n\n')
-    }
-  });
-}
-
-function returnQuery(callback, id){
-  var curDate = new Date();
-  var datetime = dateformat(curDate, 'yyyy-mm-dd hh:MM:ss');
-  var sql = "UPDATE tbl_ItemLog SET DateReturned=?, hasReturned=? WHERE ItemID=?"
-    + ";UPDATE tbl_Item SET isAvailable=? WHERE ItemID=?";
-  var inserts = [datetime, true, id, true, id];
-  sql = mysql.format(sql, inserts);
-
-  pool.getConnection(function(err, connection){
-    if(!err){
-      connection.query(sql, function(err){
-        callback(err);
-      });
-      connection.release();
-    } else{
-      console.log('Error connecting to database...\n\n');
-    }
-  });
-}
-
-function editItemQuery(callback, req){
-  var sql = "UPDATE tbl_Item SET ItemType=?, ItemName=?, AdditionalInfo=? WHERE ItemID=?"
-  var inserts = [req.body.itemType, req.body.itemName, req.body.additionalInfo, req.params.id];
-  sql = mysql.format(sql, inserts);
-
-  pool.getConnection(function(err, connection){
-    if(!err){
-      connection.query(sql, function(err){
-        callback(err);
-      });
-      connection.release();
-    } else{
-      console.log('Error connecting to database...\n\n');
+      console.log("Error connecting to database...\n\n")
     }
   });
 }
