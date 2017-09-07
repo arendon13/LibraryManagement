c362e889d9b576198bfdf765ccc159e1d8d49b24
arendon094 <arendon094@gmail.com>
2017-09-06 21:52:56 -0700

    Cleaned up file by reducing the amount of repeated code

--- LibraryServer/controllers/fetch.js
+++ LibraryServer/controllers/fetch.js
@@ -3,26 +3,34 @@ const mysql = require('mysql');
 
 const pool = mysql.createPool(config.database);
 
-// TODO: Clean up file by reducing the amount of repeated code
-
 exports.items = function(req, res, next){
-  queryItems(function(err, queryResult){
+  var sql = "SELECT * FROM tbl_Item";
+
+  querySQL(sql, function(err, queryResult){
     if(err) { return next(err); }
 
-    res.send({ 'result': queryResult });
+    res.send({ 'result': queryResult })
   });
 }
 
 exports.item = function(req, res, next){
-  queryItem(function(err, result){
+  const id = req.params.id;
+
+  var sql = "SELECT * FROM tbl_Item WHERE ItemID=?";
+  var inserts = [id];
+  sql = mysql.format(sql, inserts);
+
+  querySQL(sql, function(err, result){
     if(err) { return next(err); }
 
     res.send({ result: result });
-  }, req.params.id);
+  });
 }
 
 exports.itemTypes = function(req, res, next){
-  queryItemTypes(function(err, result){
+  var sql = "SELECT * FROM tbl_ItemType";
+
+  querySQL(sql, function(err, result){
     if(err) { return next(err); }
 
     res.send({ result: result });
@@ -30,64 +38,20 @@ exports.itemTypes = function(req, res, next){
 }
 
 exports.itemLogs = function(req, res, next){
-  queryItemLogs(function(err, queryResult){
-    if(err) { return next(err); }
-
-    res.send({ 'result': queryResult })
-  }, req.params.id)
-}
-
-function queryItems(callback){
-  pool.getConnection(function(err, connection){
-    if(!err){
-      connection.query("SELECT * FROM tbl_Item", function(err, rows){
-        callback(err, rows);
-      });
-      connection.release();
-    } else{
-      console.log("Error connecting to database ... \n\n");
-    }
-  });
-}
-
-function queryItemLogs(callback, id){
+  const id = req.params.id;
   var sql = "SELECT item.* FROM tbl_Item item WHERE item.ItemID=?;SELECT itemLog.* FROM tbl_ItemLog itemLog WHERE itemLog.ItemID=?";
   var inserts = [id, id];
   sql = mysql.format(sql, inserts);
-  var options = { sql: sql, nestTables: true}
+  var options = { sql: sql, nestTables: true};
 
-  pool.getConnection(function(err, connection){
-    if(!err){
-      connection.query(options, function(err, rows){
-        callback(err, rows);
-      });
-      connection.release();
-    } else{
-      console.log("Error connecting to database ... \n\n");
-    }
+  querySQL(options, function(err, queryResult){
+    if(err) { return next(err); }
+
+    res.send({ 'result': queryResult })
   });
 }
 
-function queryItem(callback, id){
-  var sql = "SELECT * FROM tbl_Item WHERE ItemID=?";
-  var inserts = [id];
-  sql = mysql.format(sql, inserts);
-
-  pool.getConnection(function(err, connection){
-    if(!err){
-      connection.query(sql, function(err, rows){
-        callback(err, rows);
-      })
-      connection.release();
-    } else{
-      console.log("Error connecting to database...\n\n");
-    }
-  });
-}
-
-function queryItemTypes(callback){
-  var sql = "SELECT * FROM tbl_ItemType"
-
+function querySQL(sql, callback){
   pool.getConnection(function(err, connection){
     if(!err){
       connection.query(sql, function(err, rows){
@@ -95,7 +59,7 @@ function queryItemTypes(callback){
       });
       connection.release();
     } else{
-      console.log("Error connecting to database...\n\n");
+      console.log("Error connecting to database...\n\n")
     }
   });
 }
