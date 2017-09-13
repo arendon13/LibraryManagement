9b904fface198c012e81095b21312c896231df3f
arendon094 <arendon094@gmail.com>
2017-09-12 21:01:36 -0700

    Added button to welcome component and made it functional.

--- Library/src/components/welcome.js
+++ Library/src/components/welcome.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-export default class Welcome extends Component {
-  render() {
-    return (
-      <div className="jumbotron jumbotron-fluid">
-        <div className="container">
-          <h1 className="display-3">Welcome!</h1>
-          <p className="lead">
-            This is an application that will allow an administrator to manage
-            their library by keeping track of what is borrowed, who borrowed it,
-            and when did they borrow it. Additionally it will keep track when
-            the items are returned. <br></br>
-            Navigate to the Library to get started.
-          </p>
-        </div>
+// Functional(dumb) Component
+
+const Welcome = (props) =>{
+  return(
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-3">Welcome!</h1>
+        <p className="lead">
+          This is an application that will allow an administrator to manage
+          their library by keeping track of what is borrowed, who borrowed it,
+          and when did they borrow it. Additionally it will keep track when
+          the items are returned.
+        </p>
+        <hr className="my-4"/>
+        <p>Navigate to the Library to get started.</p>
+        <p className="lead">
+          <Link className="btn btn-primary btn-lg" to="/library">Library List</Link>
+        </p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Welcome;
