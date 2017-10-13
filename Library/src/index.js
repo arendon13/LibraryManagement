c8d8b52b38da04c4ed2f730a30e3205a18f9d76f
arendon094 <arendon094@gmail.com>
2017-10-12 21:37:01 -0700

    Added to do's

--- Library/src/index.js
+++ Library/src/index.js
@@ -16,6 +16,12 @@ import CheckOut from './containers/check_out';
 import AddItemTypes from './containers/add_item_types';
 import EditItem from './containers/edit_item';
 
+// TODO: Make a Sign In component; wire it up using actions.
+// TODO: Add new reducer for signing in
+// TODO: Make Signout component
+// TODO: Add A HOC for FE sign in validation
+// TODO: Edit index.js so it can use the HOC and keep track of a token
+
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 ReactDOM.render(
